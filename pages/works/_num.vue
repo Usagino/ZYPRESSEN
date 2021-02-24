1b6386eab5e638e9bd855b3479d948c08172bbe1
itoh <yuki@frontartgraph.com>
2021-02-24 11:51:12 +0900

    add: next animation

--- pages/works/_num.vue
+++ pages/works/_num.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
-  .container(ref="workContainer")
+  .container(ref="workContainer" @mousemove="mousemoveNext")
     .container__back-blur.offset-pos
+    .next-thumbnail
+      img.next-thumbnail__image(src="https://images.microcms-assets.io/protected/ap-northeast-1:5f222472-49ae-43ef-9009-85b89c98d6be/service/uchida/media/20201203_1.jpeg?auto=compress&h=500&w=500&fit=clip")
     .work
       .work__info
-        h2.work__title Works Title
+        h2.work__title
+          .work__title__wrap
+            span Works
+          .work__title__wrap
+            span Title
         p.work__text.work__text--first Created : Mar.6.2020
         p.work__text Client : Zypressen
         a.work__text.work__text--link(href="http://yudouhu.org/" target="_blank") View Project
@@ -15,7 +21,10 @@
         img(src="https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2855&q=80").work__gallery__image
         img(src="https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2855&q=80").work__gallery__image
         img(src="https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2855&q=80").work__gallery__image
-    .next-work
+    .next-work(
+      @mouseover="mouseoverNext"
+      @mouseleave="mouseleaveNext"
+      )
       n-link.next-work__wrap(to="/works/0")
         p.next-work__text(v-for="item of 8") Next
     baseFooter
@@ -28,7 +37,12 @@ import { ScrollTrigger } from 'gsap/dist/ScrollTrigger.min.js'
 
 export default {
   data() {
-    return {}
+    return {
+      mouseOffset: {
+        offsetY: 0,
+        clientY: 0,
+      },
+    }
   },
   mounted() {
     gsap.registerPlugin(ScrollTrigger)
@@ -36,6 +50,42 @@ export default {
   },
   created() {},
   methods: {
+    mouseoverNext() {
+      console.log('mouseoverNext')
+      gsap.to('.next-thumbnail__image', {
+        duration: 0.3,
+        clipPath: 'inset(0 0% 0 0%)',
+      })
+    },
+    mouseleaveNext() {
+      console.log('mouseleave')
+      const tl = gsap.timeline()
+      tl.to('.next-thumbnail__image', {
+        duration: 0.3,
+        clipPath: 'inset(0 0% 0 100%)',
+      })
+      tl.set('.next-thumbnail__image', {
+        clipPath: 'inset(0 100% 0 0%)',
+      })
+    },
+    mousemoveNext(e) {
+      if (e) {
+        const currentOffsetY = this.$('.next-work').offsetTop
+        this.mouseOffset.clientY = e.clientY
+        gsap.to('.next-thumbnail', {
+          x: e.clientX - 100,
+          y: e.clientY - 150 + this.mouseOffset.offsetY,
+        })
+      } else {
+        gsap.to('.next-thumbnail', {
+          y: this.mouseOffset.clientY - 150 + this.mouseOffset.offsetY,
+        })
+      }
+    },
+    addMousemove() {
+      // const getOffsetY
+    },
+
     scrollCustom() {
       Scrollbar.destroyAll()
       const el = this.$refs.workContainer
@@ -44,6 +94,8 @@ export default {
         delegateTo: document,
       })
       this.bodyScrollBar.addListener(({ offset }) => {
+        this.mouseOffset.offsetY = offset.y
+        this.mousemoveNext()
         if (el.querySelector('.offset-pos')) {
           gsap.set('.offset-pos', {
             x: offset.x,
@@ -70,6 +122,16 @@ export default {
 </script>
 
 <style lang="sass" scoped>
+.next-thumbnail
+  position: fixed
+  top: 0
+  left: 0
+  z-index: 2
+  .next-thumbnail__image
+    width: 200px
+    height: 300px
+    clip-path: inset(0 100% 0 0%)
+
 .work
   +default-width(80px,1200px)
   padding: 200px 0 240px
@@ -83,12 +145,17 @@ export default {
     +sp-view
       width: 50vw
   .work__title
-    font-size: 144px
-    font-weight: 300
-    line-height: 100%
-    letter-spacing: 1.4px
+    *
+      display: block
+      font-size: 144px
+      font-weight: 300
+      line-height: 100%
+      letter-spacing: 1.4px
+    .work__title__wrap
+      overflow: hidden
     +sp-view
-      font-size: 40px
+      *
+        font-size: 40px
   .work__text
     font-weight: 300
     padding-top: 24px
