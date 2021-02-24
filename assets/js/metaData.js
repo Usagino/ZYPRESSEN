0f2e566245c1ce66ba546ea8a234a78f01c91089
itoh <yuki@frontartgraph.com>
2021-02-24 10:43:22 +0900

    edit: meta description

--- assets/js/metaData.js
+++ assets/js/metaData.js
@@ -1,6 +1,7 @@
 export default function metaData(host) {
   const data = {
-    description: '',
+    description: `ZYPRESSEN is a creative team that creates digital product designs. We don't believe in stereotypes, and each member of our team is always working with a cutting-edge philosophy. In order to create something new and different, we observe and absorb what our predecessors have built and find new values that have never been seen before.
+We call this "Different from Others".`,
     image: host + 'OGP.png',
     title: 'ZYPRESSEN',
     url: host,
